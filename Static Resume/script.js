76f771f9161c3b46d1371086a71e1153e3ca1ec6
Sana Memon <163691895+Sana-Abid46@users.noreply.github.com>
2024-09-14 22:09:29 +0500

    milestone-1

---
+++ Static Resume/script.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById('toggle-btn');
+var skills = document.getElementById('skills');
+btn.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
